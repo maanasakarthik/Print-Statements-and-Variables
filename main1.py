7ede47612548d7ccfc20e7f2bd8750831481d9e2
maanasakarthik <maanasakarthik1@gmail.com>
2026-05-18 18:30:28 +0530

    Creating different Variables

---
+++ main1.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 12
+name = "Maanasa"
+
+print(x)
+print(name)
+
+name_1 = input("Please enter your first name:")
+
+print("Hello" , name_1 , "\n Welcome to codingal")
