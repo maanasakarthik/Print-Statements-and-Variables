f097e2bdedc6e685c2e9ca68ea05d04c78132c7e
maanasakarthik <maanasakarthik1@gmail.com>
2026-05-18 18:18:44 +0530

    Printing different statements

---
+++ main.py
@@ -0,0 +1,11 @@
+#printing various statements
+print("Welcome to the Coding World!!!")
+
+#printing a number
+print(12)
+
+#print in next line
+print("Hello world \n")
+
+#passing multiple arguments
+print("hello", 20)
